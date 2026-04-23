d783215ae653561c9c6b7dbe3707d933b9b62682
Islam Hamdi <archtesla.engineer@gmail.com>
2026-04-23 19:32:05 +0200

    Create Commit.h
    Add file that named commit.h

---
+++ Commit.h
@@ -0,0 +1,21 @@
+#ifndef COMMIT_H
+#define COMMIT_H
+
+#include <string>
+#include <ctime>
+#include <iostream>
+
+class Commit {
+public:
+    int id;
+    std::string message;
+    std::string timestamp;
+    
+    Commit(int id, const std::string& msg);
+    void print() const;
+    
+    
+    bool operator==(const Commit& other) const { return id == other.id; }
+};
+
+#endif
